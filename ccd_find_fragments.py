c6d17da7baec6866abddd097e54004bd512413a0
Oliver Smart <osmart@smartsci.uk>
2016-07-16 13:22:38 +0100

    revise dictionary name and read from file, using splitlines.
    output:
    
    /home/osmart/anaconda/envs/my-rdkit-env/bin/python /home/osmart/ccd_utils/ccd_find_fragments.py
    ccd_find_fragments
    DEBUG:Have loaded 87 smiles, fragment names:
    DEBUG:{'C(C(COP(O)(O)O)O)O\t': 'glycerophos',
     'C1(=*)NC(=O)CC(=O)N1\t': 'barbit',
     'C1(C(C(OC1)CO)O)O\t': 'ribose',
     'C1(NC2C(N1)CSC2)O\t': 'biotin',
     'C1=CC(C(=C1O)CCCC)CCCC\t': 'prosto',
     'C1=CCCC(=C1)C=CC=CC=CC=C\t': 'vitaminAcore',
     'C1=CNC(NC1N)O': 'cytosine',
     'C1=COC=CC1\t': 'pyran',
     'C1=COC=CC=C1\t': 'oxepin',
     'C1=CSC=CC=C1\t': 'thiepin',
     'C1C(=O)NC(=O)*1\t': 'barbiturgroup',
     'C1C(=O)NC(=O)C1\t': 'succinimide',
     'C1C(=O)NC(=O)C1N\t': 'barbiturates',
     'C1C(=O)NC(=O)N1\t': 'acetylurea',
     'C1C(=O)NC(=O)O1\t': 'oxazolidinedione',
     'C1C(C(OC1)CO)O\t': 'deoxyribose',
     'C1C(N2C1SCCC2)O\t': 'cephalosporin',
     'C1CC1\t': 'cyclopropane',
     'C1CCC1\t': 'cyclobutane',
     'C1CCC2C(C1)C1C(CC2)C2C(CC1)CCC2\t': 'steroid',
     'C1CCCC1\t': 'cyclopentane',
     'C1CCCCC1\t': 'cyclohexane',
     'C1COC(C1)O': 'furanose',
     'C1COC(CC1)O': 'pyranose',
     'N12C(SCC1)CC2=O\t': 'penicillin',
     'N1CCNCC1\t': 'piperazine',
     'O=CCN\t': 'peptide',
     'S1CCCS1\t': 'dithiolane',
     '[nH]1cnc2c1cncn2\t': 'purine',
     'c1(nc(c2c(n1)nc[nH]2)O)N': 'guanine',
     'c1(nc(c2c(n1)nccn2)O)N\t': 'pteroyl',
     'c12c(CCO1)cccc2\t': 'benzofuran',
     'c12c(c3c([nH]1)cccc3)cccc2': 'carbazole',
     'c12c(c3c(o1)cccc3)cccc2': 'dibenzofuran',
     'c12c(c3c(s1)cccc3)cccc2': 'dibenzothiophen',
     'c12c(cc[nH]1)cccc2\t': 'indole',
     'c12c(ccs1)cccc2\t': 'benzothiophen',
     'c12nc[nH]c1cccc2': 'benzimidazole',
     'c12ocnc1cccc2': 'benzoxazole',
     'c12scnc1cccc2': 'benzothiazole',
     'c1c([nH]c(c1)Cc1ccc([nH]1)Cc1ccc[nH]1)Cc1ccc[nH]1\t': 'bilirubin',
     'c1c[nH]cn1': 'imidazole',
     'c1cc2=Cc3[nH]c(cc3)C=c3[nH]c(=Cc4[nH]c(C=c1[nH]2)cc4)cc3\t': 'porphin',
     'c1cc2c(cc1)cncc2\t': 'isoquinoline',
     'c1cc2c(cc1)cnnc2\t': 'phthalazine',
     'c1cc2c(cc1)nccc2\t': 'quinoline',
     'c1cc2c(cc1)nncc2\t': 'cinnoline',
     'c1cc[nH]n1': 'pyrazole',
     'c1ccc2c(c1)C(c1c(N2)cccc1)O\t': 'acridone',
     'c1ccc2c(c1)CC1C3C2(CCCC3)CCN1': 'alkaloid',
     'c1ccc2c(c1)CCCO2\t': 'tocol',
     'c1ccc2c(c1)Cc1c(O2)cccc1\t': 'xanthen',
     'c1ccc2c(c1)NC=CN=C2\t': 'benzodiazepine',
     'c1ccc2c(c1)Nc1c(O2)cc(c(c1)N)O\t': 'actinophenoxazine',
     'c1ccc2c(c1)Nc1c(S2)cccc1\t': 'phenothiazine',
     'c1ccc2c(c1)OC(C=C2O)O\t': 'coumarine',
     'c1ccc2c(c1)SNC=N2\t': 'benzothiadiazide',
     'c1ccc2c(c1)c1c(n2)c2n(cc1)cc1c(c2)cccc1\t': 'rauwolfia',
     'c1ccc2c(c1)ccc1c2cccc1\t': 'phenanthrene',
     'c1ccc2c(c1)cccc2\t': 'napthalene',
     'c1ccc2c(c1)cncn2\t': 'quinazoline',
     'c1ccc2c(c1)nc1c(c2)cccc1\t': 'acridine',
     'c1ccc2c(c1)nc1c(n2)c(nc(n1)O)O\t': 'flavin',
     'c1ccc2c(c1)nc1c(n2)cccc1\t': 'phenazine',
     'c1ccc2c(c1)ncc1c2cccc1\t': 'phenanthridine',
     'c1ccc2c(c1)nccn2\t': 'quinoxaline',
     'c1ccc[nH]1\t': 'pyrole',
     'c1cccc2c1C=CCO2\t': 'chromen',
     'c1cccc2c1ccc1c2cncc1\t': 'benzoisoquinoline',
     'c1ccccc1\t': 'phenyl',
     'c1ccccn1\t': 'pyridine',
     'c1ccco1\t': 'furan',
     'c1cccs1\t': 'thiophen',
     'c1ccno1': 'isoxazole',
     'c1cnc2c(c1)ccc1c2nccc1\t': 'phenanthroline',
     'c1cnc2c(c1)nccc2\t': 'naphtyridine',
     'c1cnccn1\t': 'pyrazine',
     'c1cncnc1\t': 'pyrimidine',
     'c1cnco1': 'oxazole',
     'c1cncs1': 'thiazole',
     'c1cnncc1\t': 'pyridazine',
     'c1nc(c2c(n1)nc[nH]2)O': 'inosine',
     'c1nc2c(nc1)ncnc2\t': 'pteridine',
     'c1nncs1': 'thiadiazole',
     'n1c(c2c(nc1)nc[nH]2)N': 'adenine',
     'n1ccno1': 'oxadiazole',
     'n1cnco1': 'isoxadiazole'}
    
    Process finished with exit code 0

--- ccd_find_fragments.py
+++ ccd_find_fragments.py
@@ -20,15 +20,13 @@ def load_smiles_from_smi_text_file():
     data_dir = os.path.join(this_script_dir, 'data')
     fragment_file_name = os.path.join(data_dir, 'smi.text')
     try:
-        open(fragment_file_name, 'r')
+        fragment_file = open(fragment_file_name, 'r')
     except IOError as err:
         print "Error cannot open fragment file {0} error is '{1}'".format(fragment_file_name, err.strerror)
         sys.exit(1)
-
-    lines = [line.strip("\n").strip("\t").strip("\r").replace("\n", "").replace("\t", "").replace("\r", "") for line in
-             open(fragment_file_name)]
-    smiles = dict(((smile, name) for name, smile in (line.split(':') for line in lines)))
-    return smiles
+    lines =  fragment_file.read().splitlines()
+    smiles_to_fragment_name = dict(((smile, name) for name, smile in (line.split(':') for line in lines)))
+    return smiles_to_fragment_name
 
 
 def main():
@@ -36,9 +34,9 @@ def main():
 
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-    smiles_2_fragment_name = load_smiles_from_smi_text_file()
-    logging.debug('Have loaded {} smiles, fragment names: '.format(len(smiles_2_fragment_name)))
-    logging.debug(pprint.pformat(smiles_2_fragment_name))
+    smiles_to_fragment_name = load_smiles_from_smi_text_file()
+    logging.debug('Have loaded {} smiles, fragment names: '.format(len(smiles_to_fragment_name)))
+    logging.debug(pprint.pformat(smiles_to_fragment_name))
 
 
 if __name__ == "__main__":
