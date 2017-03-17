f97dbd5704ad69b3d035d7c0f73b0625b690b4e5
Oliver Smart <osmart@smartsci.uk>
2017-03-17 18:36:37 +0000

    start hacking idea of having a chemical component class that can use either Glens or the RCSB parse - this is stub class

---
+++ pdb_chemical_components.py
@@ -0,0 +1,24 @@
+class PdbChemicalComponents(object):
+    def __init__(self):
+        pass
+
+    def read_ccd_from_cif_file(self, file_name):
+        """
+        reads the ccd from a cif file
+
+        Args:
+            file_name (str): the filename
+
+        Returns:
+            None
+        """
+        raise NotImplementedError('to be written')
+
+    def number_atoms(self):
+        """
+        gives the number of atoms in the ccd
+
+        Returns:
+            int: the number of atoms
+        """
+        raise NotImplementedError('to be written')
