d3b2305bfa013945a6adc24ffe933f8d2e2a2903
Oliver Smart <osmart@smartsci.uk>
2017-03-19 21:01:34 +0000

    work out interface idea using carbon monoxide CMO as a hard coded

--- pdb_chemical_components.py
+++ pdb_chemical_components.py
@@ -1,17 +1,32 @@
 import os
+from collections import namedtuple
+
 
 class PdbChemicalComponents(object):
+
     def __init__(self, file_name=None):
         self.chem_comp_id = None
         self.chem_comp_name = None
-        self.number_of_atoms = 0
-        self.atom_id = []
+        self.Atom = namedtuple('Atom', 'atom_id pdbx_stereo_config xyz_ideal')
+        self.atoms = []
         if file_name is not None:
             self.read_ccd_from_cif_file(file_name)
 
+    @property
+    def atom_ids(self):
+        atom_ids = []
+        for atom in self.atoms:
+            atom_ids.append(atom.atom_id)
+        return atom_ids
+
+    @property
+    def number_atoms(self):
+        return len(self.atoms)
+
+
     def read_ccd_from_cif_file(self, file_name):
         """
-        reads the ccd from a cif file
+        reads the ccd from a cif fileC
 
         Args:
             file_name (str): the filename
@@ -22,3 +37,21 @@ class PdbChemicalComponents(object):
         if not os.path.isfile(file_name):
             raise ValueError('cannot read chemical compoents from %s as file not found' % file_name)
         pass  # TODO write method
+
+    def test_hard_code_CMO(self):
+        """
+        stub to produce a hard coded carbon monoxide ccd object for development idea/testing
+
+        Returns:
+            None
+        """
+        # _chem_comp.id                                    CMO
+        self.chem_comp_id = 'CMO'
+        # _chem_comp.name                                  "CARBON MONOXIDE"
+        self.chem_comp_name = 'CARBON MONOXIDE'
+        # CMO C C C -1 1 N N N -0.296 8.526 17.112 0.607  0.000 0.000 C CMO 1
+        # CMO O O O 1  1 N N N 0.023  7.997 18.053 -0.600 0.000 0.000 O CMO 2
+        this_atom = self.Atom(atom_id='C', pdbx_stereo_config='N', xyz_ideal=(0.607, 0.000, 0.000))
+        self.atoms.append(this_atom)
+        this_atom = self.Atom(atom_id='O', pdbx_stereo_config='N', xyz_ideal=(-0.600, 0.000, 0.000))
+        self.atoms.append(this_atom)
--- test_pdb_chemical_components.py
+++ test_pdb_chemical_components.py
@@ -6,21 +6,32 @@ from pdb_chemical_components import PdbChemicalComponents
 
 class MyTestCase(unittest.TestCase):
 
-    def test_load_EOH_cif_chem_comp_id_is_EOH(self):
+    def setUp(self):
         eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        ccd = PdbChemicalComponents(file_name=eoh_cif_file)
-        self.assertEqual('EOH', ccd.chem_comp_id)
+        self.ccd_eoh = PdbChemicalComponents(file_name=eoh_cif_file)
+        self.ccd_hard_code_cmo = PdbChemicalComponents()
+        self.ccd_hard_code_cmo.test_hard_code_CMO()
 
-    def test_load_EOH_cif_atom_ids_are_correct(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        ccd = PdbChemicalComponents(file_name=eoh_cif_file)
-        self.assertEqual(['C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'], ccd.chem_comp_id)
+    def tearDown(self):
+        pass
 
-    def test_load_EOH_number_atoms_is_9(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        ccd = PdbChemicalComponents()
-        ccd.read_ccd_from_cif_file(eoh_cif_file)
-        self.assertEqual(9, ccd.number_of_atoms)
+    def test_hard_code_CMO_chem_comp_id_is_CMO(self):
+        self.assertEqual('CMO', self.ccd_hard_code_cmo.chem_comp_id)
+
+    def test_hard_code_CMO_atom_ids(self):
+        self.assertEqual(['C', 'O'], self.ccd_hard_code_cmo.atom_ids)
+
+    def test_hard_code_CMO_has_2_atoms(self):
+        self.assertEqual(2, self.ccd_hard_code_cmo.number_atoms)
+
+    # def test_load_EOH_cif_chem_comp_id_is_EOH(self):
+    #     self.assertEqual('EOH', self.ccd_eoh.chem_comp_id)
+    #
+    # def test_load_EOH_cif_atom_ids_are_correct(self):
+    #     self.assertEqual(['C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'], self.ccd_eoh.atomid)
+    #
+    # def test_load_EOH_number_atoms_is_9(self):
+    #     self.assertEqual(9, self.ccd_eoh.number_of_atoms)
 
 
 if __name__ == '__main__':
