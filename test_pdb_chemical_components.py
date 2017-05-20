597da5c4215c943c2ea700de2a1668fd6ecb5646
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-05-20 13:12:58 +0100

    revise unit tests so the mmcifIO and CifFile parser returns are both tested if there is not an import error.
    Output:
    
            $ nosetests -v test_pdb_chemical_components.py
            test_CifFile_parser_loads (test_pdb_chemical_components.MyTestCase) ... ok
            test_hard_code_CMO_atom_ids (test_pdb_chemical_components.MyTestCase) ... ok
            test_hard_code_CMO_chem_comp_id_is_CMO (test_pdb_chemical_components.MyTestCase) ... ok
            test_hard_code_CMO_chem_comp_name (test_pdb_chemical_components.MyTestCase) ... ok
            test_hard_code_CMO_has_2_atoms (test_pdb_chemical_components.MyTestCase) ... ok
            test_load_EOH_cif_atom_ids_are_correct (test_pdb_chemical_components.MyTestCase) ... ok
            test_load_EOH_cif_chem_comp_id_is_EOH (test_pdb_chemical_components.MyTestCase) ... ok
            test_load_EOH_cif_chem_comp_name (test_pdb_chemical_components.MyTestCase) ... ok
            test_load_EOH_number_atoms_is_9 (test_pdb_chemical_components.MyTestCase) ... ok
            test_mmcifIO_parser_loads (test_pdb_chemical_components.MyTestCase) ... ok
    
            ----------------------------------------------------------------------
            Ran 10 tests in 0.106s
    
            OK

--- test_pdb_chemical_components.py
+++ test_pdb_chemical_components.py
@@ -10,11 +10,6 @@ class MyTestCase(unittest.TestCase):
         self.cmo = PdbChemicalComponents()
         self.cmo.load_carbon_monoxide_hard_coded()
 
-    def load_EOH_cif(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        self.ccd_eoh = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='CifFile')
-        #self.ccd_eoh = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='mmcifIO')
-
     def test_hard_code_CMO_chem_comp_id_is_CMO(self):
         self.load_carbon_monoxide_hard_coded()
         self.assertEqual('CMO', self.cmo.chem_comp_id)
@@ -31,22 +26,54 @@ class MyTestCase(unittest.TestCase):
         self.load_carbon_monoxide_hard_coded()
         self.assertEqual(2, self.cmo.number_atoms)
 
+    def load_EOH_cif(self):
+        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
+        try:
+            self.ccd_eoh_from_ciffile = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='CifFile')
+        except ImportError:
+            self.ccd_eoh_from_ciffile = None
+        try:
+            self.ccd_eoh_from_mmccifio = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='mmcifIO')
+        except ImportError:
+            self.ccd_eoh_from_mmccifio = None
+
+    def test_CifFile_parser_loads(self):
+        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
+        try:
+            PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='CifFile')
+        except ImportError:
+            self.fail('failed to load module for CifFile parser other tests will not test this.')
+
+    def test_mmcifIO_parser_loads(self):
+        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
+        try:
+            PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='mmcifIO')
+        except ImportError:
+            self.fail('failed to load module for mmcifIO parser other tests will not test this.')
+
     def test_load_EOH_cif_chem_comp_id_is_EOH(self):
         self.load_EOH_cif()
-        self.assertEqual('EOH', self.ccd_eoh.chem_comp_id)
+        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
+            if eoh_from_cif is not None:
+                self.assertEqual('EOH', eoh_from_cif.chem_comp_id)
 
     def test_load_EOH_cif_chem_comp_name(self):
         self.load_EOH_cif()
-        self.assertEqual('ETHANOL', self.ccd_eoh.chem_comp_name)
+        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
+            if eoh_from_cif is not None:
+                self.assertEqual('ETHANOL', eoh_from_cif.chem_comp_name)
 
     def test_load_EOH_cif_atom_ids_are_correct(self):
         self.load_EOH_cif()
-        self.assertEqual(('C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'), self.ccd_eoh.atom_ids)
+        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
+            if eoh_from_cif is not None:
+                self.assertEqual(('C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'), eoh_from_cif.atom_ids)
 
     def test_load_EOH_number_atoms_is_9(self):
         self.load_EOH_cif()
-        self.assertEqual(9, self.ccd_eoh.number_atoms)
-
+        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
+            if eoh_from_cif is not None:
+                self.assertEqual(9, eoh_from_cif.number_atoms)
 
 if __name__ == '__main__':
     unittest.main()
