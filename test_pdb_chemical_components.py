350cbe472fa428b66ea04ec17be1698be1318a73
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-05-21 12:27:33 +0100

    refactor the unit tests to use nose tools in place of unit test.
    The use of test generators allows for a much more readable code and output
    http://nose.readthedocs.io/en/latest/writing_tests.html#test-generators

--- test_pdb_chemical_components.py
+++ test_pdb_chemical_components.py
@@ -14,108 +14,78 @@
 # KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
+from nose.tools import assert_equals
 import os
-import unittest
 from pdb_chemical_components import PdbChemicalComponents
 
 
-class MyTestCase(unittest.TestCase):
+def load_carbon_monoxide_hard_coded():
+    cmo = PdbChemicalComponents()
+    cmo.load_carbon_monoxide_hard_coded()
+    return cmo
 
-    def load_carbon_monoxide_hard_coded(self):
-        self.cmo = PdbChemicalComponents()
-        self.cmo.load_carbon_monoxide_hard_coded()
 
-    def test_hard_code_CMO_chem_comp_id_is_CMO(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual('CMO', self.cmo.chem_comp_id)
+def test_hard_code_cmo_chem_comp_id_is_cmo():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals('CMO', cmo.chem_comp_id)
 
-    def test_hard_code_CMO_chem_comp_name(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual('CARBON MONOXIDE', self.cmo.chem_comp_name)
 
-    def test_hard_code_CMO_inchikey(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual('UGFAIRIUMAVXCW-UHFFFAOYSA-N', self.cmo.inchikey)
+def test_hard_code_cmo_chem_comp_name():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals('CARBON MONOXIDE', cmo.chem_comp_name)
 
-    def test_hard_code_CMO_atom_ids(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual(('C', 'O'), self.cmo.atom_ids)
 
-    def test_hard_code_CMO_has_2_atoms(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual(2, self.cmo.number_atoms)
+def test_hard_code_cmo_inchikey():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals('UGFAIRIUMAVXCW-UHFFFAOYSA-N', cmo.inchikey)
 
-    def test_hard_code_CMO_has_1_bond(self):
-        self.load_carbon_monoxide_hard_coded()
-        self.assertEqual(1, self.cmo.number_bonds)
 
-    def test_hard_code_CMO_bond_atom_ids(self):
-        self.load_carbon_monoxide_hard_coded()
-        the_bond = self.cmo.bonds[0]
-        self.assertEqual('C', the_bond.atom_id_1)
-        self.assertEqual('O', the_bond.atom_id_2)
+def test_hard_code_has_2_atoms():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals(2, cmo.number_atoms)
 
-    def load_EOH_cif(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
+
+def test_hard_code_cmo_atom_ids():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals(('C', 'O'), cmo.atom_ids)
+
+
+def test_hard_code_has_1_bond():
+    cmo = load_carbon_monoxide_hard_coded()
+    assert_equals(1, cmo.number_bonds)
+
+
+def test_hard_code_bond_atom_ids():
+    cmo = load_carbon_monoxide_hard_coded()
+    the_bond = cmo.bonds[0]
+    assert_equals('C', the_bond.atom_id_1)
+    assert_equals('O', the_bond.atom_id_2)
+
+
+def eoh_cif_file():
+    return os.path.join('data', 'cif', 'EOH.cif')
+
+
+def test_ciffile_parser_loads():
+    try:
+        PdbChemicalComponents(file_name=eoh_cif_file(), cif_parser='CifFile')
+    except ImportError as msg:
+        assert False, 'Failed to load module for CifFile parser "{}". ' \
+                      'Other tests will skip using testing this parser'.format(msg)
+
+
+def test_load_eoh_from_cif():
+    for cif_parser in 'mmcifIO', 'CifFile':
         try:
-            self.ccd_eoh_from_ciffile = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='CifFile')
+            eoh = PdbChemicalComponents(file_name=eoh_cif_file(), cif_parser=cif_parser)
+            description = ', with cif_parser={}'.format(cif_parser)
+            yield assert_equals, 'EOH', eoh.chem_comp_id, 'chem_comp_id' + description
+            yield assert_equals, 'ETHANOL', eoh.chem_comp_name, 'chem_comp_name' + description
+            yield assert_equals, 'LFQSCWFLJHTTHZ-UHFFFAOYSA-N', eoh.inchikey, 'inchikey' + description
+            yield assert_equals, 9, eoh.number_atoms, 'number_atoms' + description
+            yield assert_equals, ('C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'), \
+                eoh.atom_ids, 'atom_ids' + description
+            yield assert_equals, 8, eoh.number_bonds, 'number_bonds' + description
         except ImportError:
-            self.ccd_eoh_from_ciffile = None
-        try:
-            self.ccd_eoh_from_mmccifio = PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='mmcifIO')
-        except ImportError:
-            self.ccd_eoh_from_mmccifio = None
-
-    def test_CifFile_parser_loads(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        try:
-            PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='CifFile')
-        except ImportError:
-            self.fail('failed to load module for CifFile parser other tests will not test this.')
-
-    def test_mmcifIO_parser_loads(self):
-        eoh_cif_file = os.path.join('data', 'cif', 'EOH.cif')
-        try:
-            PdbChemicalComponents(file_name=eoh_cif_file, cif_parser='mmcifIO')
-        except ImportError:
-            self.fail('failed to load module for mmcifIO parser other tests will not test this.')
-
-    def test_load_EOH_cif_chem_comp_id_is_EOH(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual('EOH', eoh_from_cif.chem_comp_id)
-
-    def test_load_EOH_cif_chem_comp_name(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual('ETHANOL', eoh_from_cif.chem_comp_name)
-
-    def test_load_EOH_cif_inchikey(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual('LFQSCWFLJHTTHZ-UHFFFAOYSA-N', eoh_from_cif.inchikey)
-
-    def test_load_EOH_cif_atom_ids_are_correct(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual(('C1', 'C2',  'O', 'H11', 'H12', 'H21', 'H22', 'H23', 'HO'), eoh_from_cif.atom_ids)
-
-    def test_load_EOH_number_atoms_is_9(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual(9, eoh_from_cif.number_atoms)
-
-    def test_load_EOH_number_bonds_is_8(self):
-        self.load_EOH_cif()
-        for eoh_from_cif in self.ccd_eoh_from_ciffile, self.ccd_eoh_from_mmccifio:
-            if eoh_from_cif is not None:
-                self.assertEqual(8, eoh_from_cif.number_bonds)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            pass
