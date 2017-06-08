2053d62ec5506a07ba5cd2b03321ee991179d2c9
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-06-08 14:21:03 +0100

    Temporary jiffy  to get the number of rotatable bonds for each PDB CCD - works
    Running script:
             $ time ./temp_jiffy_number_rotatable_bonds_jiffy.py > id_nrot.csv
             [14:16:50] SMILES Parse Error: syntax error for input: '[Ru]|1|2|3|4|5|6|7|8(|[CH]9[C]|1=[C]|2[C]|3=[C]|49)|[C]%10=[C]|5[C]|6([C]|7=[C]|8%10)c%11cn(nn%11)c%12ccc(cc%12)[S](N)(=O)=O'
            [14:16:50] Explicit valence for atom # 23 Be, 4, is greater than permitted
            [14:16:51] SMILES Parse Error: syntax error for input: 'O=C1NC(=O)c2cc3c([Ru]|4|5(|S6CCS|4CCS|5CC6)(N=C=S)|n7ccccc37)cc12'
            [14:16:51] SMILES Parse Error: syntax error for input: '[Pt+]|1|2(|NCCN|1)|I[Pt+]|3(|NCCN|3)I|2'
            ....
            [14:17:09] SMILES Parse Error: syntax error for input: 'CC1=C(CCC(O)=O)C2=N3|[Zn]45|N6=C(C=c7n4c(=C2)c(CCC(O)=O)c7C)C(=C(C=C)C6=Cc8n5c(C=C13)c(C=C)c8C)C'
            [14:17:09] SMILES Parse Error: syntax error for input: 'O|[Zn++]|O'
            [14:17:09] SMILES Parse Error: syntax error for input: 'O|[Zn++](|O)|O'
            [14:17:09] SMILES Parse Error: syntax error for input: '[Cl-]|[Pt++]|1(|[Cl-])(|[Cl-])|C=C|1'
    
            real    0m19.738s
            user    0m19.362s
            sys     0m0.209s
    
    Get 349 errors out of 24312

--- temp_jiffy_number_rotatable_bonds_jiffy.py
+++ temp_jiffy_number_rotatable_bonds_jiffy.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
+# temporary jiffy to calculate number of rotatable bonds for each chemical component -
+# use SMILES parsed from  http://ftp.ebi.ac.uk/pub/databases/msd/pdbechem/chem.xml
+# write to csv with -1
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 try:
     import xml.etree.cElementTree as ETree
 except ImportError:
     import xml.etree.ElementTree as ETree
-tree = ETree.ElementTree(file='temp_sample_chem_comp.xml')
+tree = ETree.ElementTree(file='/nfs/ftp/pub/databases/msd/pdbechem/chem.xml')
 root = tree.getroot()
 for chem_comp in root:
     comp_id = chem_comp.find('id').text
-    smiles = chem_comp.find('stereoSmiles').text
-    nrb = Descriptors.NumRotatableBonds(Chem.MolFromSmiles(smiles))
-    print 'debug id: {} smiles: {} nrb: {}'.format(comp_id, smiles, nrb)
+    smiles = chem_comp.find('stereoSmiles')
+    nrb = -1  # means there is an error
+    if smiles is not None:
+        smiles = smiles.text
+        rdkit_mol = Chem.MolFromSmiles(smiles)
+        if rdkit_mol is not None:
+            nrb = Descriptors.NumRotatableBonds(Chem.MolFromSmiles(smiles))
+    print '{},{}'.format(comp_id, nrb)
