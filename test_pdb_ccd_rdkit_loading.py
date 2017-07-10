4d3cc0b09ea49e74c6e53902d4746e5017250246
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-07-10 16:48:18 +0100

    clean up tests - previously test_write_sdf also tested inchikeys seperate the two out to test_pdb_ccd_rdkit_loading.py and test_write_sdf.py
    output:
    (new-rdkit-env) [osmart@ebi-cli-001 ccd_utils]$ nosetests  test_pdb_ccd_rdkit_loading.py
    ...........[16:39:42] WARNING: Proton(s) added/removed
    F...[16:39:42] WARNING: Metal was disconnected
    ....[16:39:42] WARNING: Metal was disconnected; Proton(s) added/removed
    F.....[16:39:42] WARNING: Proton(s) added/removed
    ....
    ======================================================================
    FAIL: test_pdb_ccd_rdkit_loading.test_inchikey_match_for_all_sample_cifs('XVTUQDWPJJBEHJ-KZCWQMDCSA-L', 'XVTUQDWPJJBEHJ-HALCAUQZSA-L', 'check inchikeys match for CDL')
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/ebi/msd/work2/osmart/conda/envs/new-rdkit-env/lib/python2.7/site-packages/nose/case.py", line 197, in runTest
        self.test(*self.arg)
    AssertionError: check inchikeys match for CDL
    
    ======================================================================
    FAIL: test_pdb_ccd_rdkit_loading.test_inchikey_match_for_all_sample_cifs('FEDYMSUPMFCVOD-UJJXFSCMSA-N', 'KABFMIBPWCXCRK-RGGAHWMASA-L', 'check inchikeys match for HEM')
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/ebi/msd/work2/osmart/conda/envs/new-rdkit-env/lib/python2.7/site-packages/nose/case.py", line 197, in runTest
        self.test(*self.arg)
    AssertionError: check inchikeys match for HEM
    
    ----------------------------------------------------------------------
    Ran 29 tests in 0.407s
    
    FAILED (failures=2)
    (new-rdkit-env) [osmart@ebi-cli-001 ccd_utils]$ nosetests  test_write_sdf.py
    ..........................................................................................................
    ----------------------------------------------------------------------
    Ran 106 tests in 0.616s
    
    OK

---
+++ test_pdb_ccd_rdkit_loading.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# software from PDBe: Protein Data Bank in Europe; http://pdbe.org
+#
+# Copyright 2017 EMBL - European Bioinformatics Institute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+import glob
+import os
+import unittest
+
+from nose.tools import assert_equals, assert_in, assert_not_equal, assert_is_instance, assert_true, assert_not_in
+from test_pdb_chemical_components import cif_filename, test_file_path_name
+from pdb_chemical_components_rdkit import PdbChemicalComponentsRDKit
+
+
+def test_hard_code_cmo():
+    cmo = PdbChemicalComponentsRDKit(cif_parser='test_hard_code_cmo')
+    yield assert_equals, 'CMO', cmo.chem_comp_id, 'chem_comp_id'
+    yield assert_equals, 'CARBON MONOXIDE', cmo.chem_comp_name, 'chem_comp_name'
+    yield assert_equals, 'UGFAIRIUMAVXCW-UHFFFAOYSA-N', cmo.inchikey, 'chem_inchikey'
+    # rdkit should be able to get an inchikey that is the same that read from the CCD cif file
+    yield assert_equals, 2, cmo.rdkit_mol.GetNumAtoms(), 'rdkit_mol.GetNumAtoms() should give 2'
+    yield assert_equals, cmo.inchikey, cmo.inchikey_from_rdkit, 'inchikey from cif file should match the rdkit inchikey'
+
+
+def test_load_eoh_from_cif():
+    eoh = PdbChemicalComponentsRDKit(file_name=cif_filename('EOH'))
+    yield assert_equals, 'EOH', eoh.chem_comp_id, 'chem_comp_id'
+    yield assert_equals, eoh.inchikey, eoh.inchikey_from_rdkit, 'inchikey from cif file should match the rdkit inchikey'
+
+
+def test_inchikey_match_for_all_sample_cifs():
+    for ciffile in supply_list_of_sample_cifs():
+        pdb_cc = PdbChemicalComponentsRDKit(file_name=ciffile)
+        yield assert_equals, pdb_cc.inchikey, pdb_cc.inchikey_from_rdkit, \
+            'check inchikeys match for ' + pdb_cc.chem_comp_id
+
+
+def supply_list_of_sample_cifs():
+    """
+    returns the list of sample pdb ccd cifs for test.
+
+    Args:
+        None
+
+    Returns:
+        list of filenames
+    """
+    return sorted(glob.glob(os.path.join(test_file_path_name, '*.cif')))
+
+
+
+
+class DummyTestCaseSoPycharmRecognizesNoseTestsAsTests(unittest.TestCase):
+    pass
--- test_write_sdf.py
+++ test_write_sdf.py
@@ -20,18 +20,13 @@ import os
 import unittest
 
 from nose.tools import assert_equals, assert_in, assert_not_equal, assert_is_instance, assert_true, assert_not_in
-from test_pdb_chemical_components import cif_filename, test_file_path_name
+from test_pdb_chemical_components import cif_filename
 from pdb_chemical_components_rdkit import PdbChemicalComponentsRDKit
+from test_pdb_ccd_rdkit_loading import supply_list_of_sample_cifs
 
 
 def test_hard_code_cmo():
     cmo = PdbChemicalComponentsRDKit(cif_parser='test_hard_code_cmo')
-    yield assert_equals, 'CMO', cmo.chem_comp_id, 'chem_comp_id'
-    yield assert_equals, 'CARBON MONOXIDE', cmo.chem_comp_name, 'chem_comp_name'
-    yield assert_equals, 'UGFAIRIUMAVXCW-UHFFFAOYSA-N', cmo.inchikey, 'chem_inchikey'
-    # rdkit should be able to get an inchikey that is the same that read from the CCD cif file
-    yield assert_equals, 2, cmo.rdkit_mol.GetNumAtoms(), 'rdkit_mol.GetNumAtoms() should give 2'
-    yield assert_equals, cmo.inchikey, cmo.inchikey_from_rdkit, 'inchikey from cif file should match the rdkit inchikey'
     sdf_string_ideal = cmo.sdf_file_or_string(ideal=True)
     yield assert_is_instance, sdf_string_ideal, str, 'sdf string must be a string'
     if isinstance(sdf_string_ideal, str):
@@ -53,8 +48,6 @@ def test_hard_code_cmo():
 
 def test_load_eoh_from_cif():
     eoh = PdbChemicalComponentsRDKit(file_name=cif_filename('EOH'))
-    yield assert_equals, 'EOH', eoh.chem_comp_id, 'chem_comp_id'
-    yield assert_equals, eoh.inchikey, eoh.inchikey_from_rdkit, 'inchikey from cif file should match the rdkit inchikey'
     sdf_string_ideal_h = eoh.sdf_file_or_string(ideal = True)
     sdf_string_model_h = eoh.sdf_file_or_string(ideal = False)
     sdf_string_ideal_no_h = eoh.sdf_file_or_string(hydrogen = False)
@@ -72,13 +65,6 @@ def test_load_eoh_from_cif():
     sdf_string_ideal_h_with_alias = eoh.sdf_file_or_string(ideal=True, alias=True)
     yield assert_in,  'H23', sdf_string_ideal_h_with_alias, \
         'sdf produce with alias=False should have an atom alias record for H23'
-
-
-def test_inchikey_match_for_all_sample_cifs():
-    for ciffile in supply_list_of_sample_cifs():
-        pdb_cc = PdbChemicalComponentsRDKit(file_name=ciffile)
-        yield assert_equals, pdb_cc.inchikey, pdb_cc.inchikey_from_rdkit, \
-            'check inchikeys match for ' + pdb_cc.chem_comp_id
 
 
 def test_sdf_write_for_all_sample_cifs():
@@ -107,18 +93,6 @@ def test_sdf_write_for_all_sample_cifs():
             '{} call to pdb_cc.sdf_file_or_string(file="{}") must create a non-empty file.'.\
             format(pdb_cc.chem_comp_id, sdf_model_no_h)
 
-def supply_list_of_sample_cifs():
-    """
-    returns the list of sample pdb ccd cifs for test.
-
-    Args:
-        None
-
-    Returns:
-        list of filenames
-    """
-    return sorted(glob.glob(os.path.join(test_file_path_name, '*.cif')))
-
 
 def file_name_in_subdir_for_output_files(file_name):
     """
@@ -138,7 +112,6 @@ def file_name_in_subdir_for_output_files(file_name):
     if os.path.isfile(out_file_name):
         os.remove(out_file_name)
     return out_file_name
-
 
 class DummyTestCaseSoPycharmRecognizesNoseTestsAsTests(unittest.TestCase):
     pass
