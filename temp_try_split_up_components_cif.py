922941e60c90fadd7e1ddd4d3c6918ca7a2cba6a
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-08-10 16:20:36 +0100

    test splitting complete components.cif
    works - for results see comment on issue #9 https://gitlab.com/pdbe/ccd_utils/issues/9#note_37281342
    
    For issue #9 Split wwPDB chemical component dictionary file into separate cif file for each component

--- temp_try_split_up_components_cif.py
+++ temp_try_split_up_components_cif.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # try reading in a cif file with 5 PDB CCD using the PDBeCIF parser - can we split it up?
 # see issue #9
+import os
 import mmCif.mmcifIO as mmcifIO
 from utilities import file_name_in_tsts_out
 cif_parser = mmcifIO.CifFileReader(input='data', preserve_order=True)
-cif_dictionary = cif_parser.read('tests/components_cif/components.cif.first_five_comps', output='cif_dictionary')
+# test full components.cif by commenting/uncommenting next 2 lines:
+test_file = 'tests/components_cif/components.cif.first_five_comps'
+test_file = '/homes/osmart/Downloads/components.cif'
+cif_dictionary = cif_parser.read(test_file, output='cif_dictionary')
 print('debug cif_obj is type {}'.format(type(cif_dictionary)))
 for data_block_id, data_block in cif_dictionary.items():
     print 'data_block_id = ', data_block_id
@@ -15,7 +19,10 @@ for data_block_id, data_block in cif_dictionary.items():
     print('chem_comp_id={} chem_comp_name={}'.format(chem_comp_id, chem_comp_name))
     just_this_data_block = {}
     just_this_data_block[chem_comp_id] = data_block
-    output_cif_filename = file_name_in_tsts_out(chem_comp_id + '_split.cif')
+    if len(cif_dictionary) < 10:
+        output_cif_filename = file_name_in_tsts_out(chem_comp_id + '_split.cif')
+    else:
+        output_cif_filename = os.path.join('/var/tmp/test', chem_comp_id + '_split.cif')
     cfd = mmcifIO.CifFileWriter(output_cif_filename, preserve_order=True)
     cfd.write(just_this_data_block)
     print('have written file {}'.format(output_cif_filename))
