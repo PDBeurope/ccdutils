a28614a440a1eb53857e85b3d30cdfbdfe2efafa
Oliver Smart <osmart@smartsci.uk>
2017-08-27 09:09:36 +0100

    Simplfy test_process_components_cif_cli.py so it works with python 2
    For issue #13 "Script to read wwPDB chemical component dictionary and split it to produce PDBeChem outputs"

--- tests/test_process_components_cif_cli.py
+++ tests/test_process_components_cif_cli.py
@@ -11,40 +11,31 @@ from process_components_cif_cli import create_parser, process_components_cif, fi
 from utilities import test_components_cif_first_file_comps, file_name_in_tsts_out
 
 
-class CommandLineTestCase():
+def test_with_empty_args():
     """
-    Base TestCase class, sets up a CLI parser
+    User passes no args, should produce a usage statement and then raise SystemExit. Usage statement will start
     """
-    @classmethod
-    def setUpClass(cls):
-        parser = create_parser()
-        cls.parser = parser
+    parser = create_parser()
+    assert_raises(SystemExit, parser.parse_args, [])
 
 
-class TestProcessComponentsCIFTestCase(CommandLineTestCase):
-    def test_with_empty_args(self):
-        """
-        User passes no args, should produce a usage statement and then raise SystemExit. Usage statement will start
-        """
-        assert_raises(SystemExit, self.parser.parse_args, [])
-
-    def test_with_components_cif_first_file_comps(self):
-        test_components_cif = test_components_cif_first_file_comps
-        test_output_dir = file_name_in_tsts_out('test_process_components_cif_cli')
-        if os.path.isdir(test_output_dir):
-            shutil.rmtree(test_output_dir)
-        chem_comp_ids = ('000', '001', '002', '003', '004')
-        args = self.parser.parse_args([test_components_cif, test_output_dir, '--debug'])
-        process_components_cif(args.COMPONENTS_CIF, args.OUTPUT_DIR,  args.debug)
-        yield assert_true, os.path.isdir(test_output_dir), 'output directory {} must be created'.format(test_output_dir)
-        files_dir = os.path.join(test_output_dir, 'files')
-        yield assert_true, os.path.isdir(test_output_dir), 'files sub-directory {} must be created'.format(files_dir)
-        for subdir in file_subdirs:
-            path = os.path.join(files_dir, subdir)
-            yield assert_true, os.path.isdir(path), '{} sub-directory {} must be created'.format(subdir, path)
-            for chem_comp_id in chem_comp_ids:
-                # simple check that there is a single file starting with the chem_comp_id
-                file_for_chem_comp_id = glob.glob1(path, chem_comp_id + '*')
-                yield assert_equal, len(file_for_chem_comp_id), 1, \
-                    'there should be a file matching {}* in {}'.format(chem_comp_id, subdir)
-
+def test_with_components_cif_first_file_comps():
+    parser = create_parser()
+    test_components_cif = test_components_cif_first_file_comps
+    test_output_dir = file_name_in_tsts_out('test_process_components_cif_cli')
+    if os.path.isdir(test_output_dir):
+        shutil.rmtree(test_output_dir)
+    chem_comp_ids = ('000', '001', '002', '003', '004')
+    args = parser.parse_args([test_components_cif, test_output_dir, '--debug'])
+    process_components_cif(args.COMPONENTS_CIF, args.OUTPUT_DIR, args.debug)
+    yield assert_true, os.path.isdir(test_output_dir), 'output directory {} must be created'.format(test_output_dir)
+    files_dir = os.path.join(test_output_dir, 'files')
+    yield assert_true, os.path.isdir(test_output_dir), 'files sub-directory {} must be created'.format(files_dir)
+    for subdir in file_subdirs:
+        path = os.path.join(files_dir, subdir)
+        yield assert_true, os.path.isdir(path), '{} sub-directory {} must be created'.format(subdir, path)
+        for chem_comp_id in chem_comp_ids:
+            # simple check that there is a single file starting with the chem_comp_id
+            file_for_chem_comp_id = glob.glob1(path, chem_comp_id + '*')
+            yield assert_equal, len(file_for_chem_comp_id), 1, \
+                  'there should be a file matching {}* in {}'.format(chem_comp_id, subdir)
