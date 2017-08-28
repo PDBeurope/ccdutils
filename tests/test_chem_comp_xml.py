3991bfae8779782162c13fe77164de2aef01981a
Oliver Smart <osmart@smartsci.uk>
2017-08-28 14:24:13 +0100

    chem_comp_xml creation for a single chemical component: extend unit_test to test GLU as well as EOH.
    For issue #10 "Create chem.xml for all components"
    
    Many of the unit_test for GLU currently fail become have not yet
    implemented required parsing methods:
    
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'GLU chem_comp_xml should contain "<chemComp>"') ... ok
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'GLU chem_comp_xml should contain "<id>GLU</id>"') ... ok
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'GLU chem_comp_xml should contain "<name>GLUTAMIC ACID</name>"') ... ok
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'GLU chem_comp_xml should contain "<formula>C5 H9 N O4</formula>"') ... ok
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(False, 'GLU chem_comp_xml should contain "<systematicName>(2S)-2-azanylpentanedioic acid</systematicName>"') ... FAIL
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(False, 'GLU chem_comp_xml should contain "<stereoSmiles>N[C@@H](CCC(O)=O)C(O)=O</stereoSmiles>"') ... FAIL
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(False, 'GLU chem_comp_xml should contain "<nonStereoSmiles>N[CH](CCC(O)=O)C(O)=O</nonStereoSmiles>"') ... FAIL
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(False, 'GLU chem_comp_xml should contain "<InChi>InChI=1S/C5H9NO4/c6-3(5(9)10)1-2-4(7)8/h3H,1-2,6H2,(H,7,8)(H,9,10)/t3-/m0/s1</InChi>"') ... FAIL
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'GLU chem_comp_xml should contain "</chemComp>"') ... ok
        tests.test_chem_comp_xml.test_chem_comp_for_eoh_and_glu(True, 'echo GLU chem_comp_xml after any failure: "<chemComp>\n  <id>GLU</id>\n  <name>GLUTAMIC ACID</name>\n  <formula>C5 H9 N O4</formula>\n  <systematicName>TODO</systematicName>\n  <stereoSmiles>TODO</stereoSmiles>\n  <nonStereoSmiles>TODO</nonStereoSmiles>\n  <InChi>TODO</InChi>\n</chemComp>\n"') ... FAIL

--- tests/test_chem_comp_xml.py
+++ tests/test_chem_comp_xml.py
@@ -21,32 +21,52 @@ from pdb_chemical_components_rdkit import PdbChemicalComponentsRDKit
 from utilities import cif_filename
 
 
-def test_chem_comp_for_eoh():
-    # record from old /nfs/ftp/pub/databases/msd/pdbechem/chem.xml
-    #   <chemComp>
-    #     <id>EOH</id>
-    #     <name>ETHANOL</name>
-    #     <formula>C2 H6 O</formula>
-    #     <systematicName>ethanol</systematicName>
-    #     <stereoSmiles>CCO</stereoSmiles>
-    #     <nonStereoSmiles>CCO</nonStereoSmiles>
-    #     <InChi>InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3</InChi>
-    #   </chemComp>
-    old_eoh_records = ('<chemComp>', '<id>EOH</id>', '<name>ETHANOL</name>', '<formula>C2 H6 O</formula>',
-                       '<systematicName>ethanol</systematicName>', '<stereoSmiles>CCO</stereoSmiles>',
-                       '<nonStereoSmiles>CCO</nonStereoSmiles>', '<InChi>InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3</InChi>',
-                       '</chemComp>')
-    eoh = PdbChemicalComponentsRDKit(file_name=cif_filename('EOH'))
-    chem_comp_xml = eoh.chem_comp_xml()
-    lines = chem_comp_xml.splitlines()
-    any_fail = False
-    for old_record in old_eoh_records:
-        match = False
-        for line in lines:
-            if old_record in line:
-                match = True
-                break
-        yield assert_true, match, 'EOH chem_comp_xml should contain "{}"'.format(old_record)
-        if not match:
-            any_fail = True
-    yield assert_false, any_fail, 'echo EOH chem_comp_xml after any failure: "{}"'.format(chem_comp_xml)
+def test_chem_comp_for_eoh_and_glu():
+    for chem_comp_id in 'EOH', 'GLU':
+        # record from old /nfs/ftp/pub/databases/msd/pdbechem/chem.xml
+        #   <chemComp>
+        #     <id>EOH</id>
+        #     <name>ETHANOL</name>
+        #     <formula>C2 H6 O</formula>
+        #     <systematicName>ethanol</systematicName>
+        #     <stereoSmiles>CCO</stereoSmiles>
+        #     <nonStereoSmiles>CCO</nonStereoSmiles>
+        #     <InChi>InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3</InChi>
+        #   </chemComp>
+        if chem_comp_id is 'EOH':
+            old_records = ('<chemComp>', '<id>EOH</id>', '<name>ETHANOL</name>', '<formula>C2 H6 O</formula>',
+                           '<systematicName>ethanol</systematicName>', '<stereoSmiles>CCO</stereoSmiles>',
+                           '<nonStereoSmiles>CCO</nonStereoSmiles>', '<InChi>InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3</InChi>',
+                           '</chemComp>')
+        else:
+            #   <chemComp>
+            #   <id>GLU</id>
+            #   <name>GLUTAMIC ACID</name>
+            #   <formula>C5 H9 N O4</formula>
+            #   <systematicName>(2S)-2-azanylpentanedioic acid</systematicName>
+            #   <stereoSmiles>N[C@@H](CCC(O)=O)C(O)=O</stereoSmiles>
+            #   <nonStereoSmiles>N[CH](CCC(O)=O)C(O)=O</nonStereoSmiles>
+            #   <InChi>InChI=1S/C5H9NO4/c6-3(5(9)10)1-2-4(7)8/h3H,1-2,6H2,(H,7,8)(H,9,10)/t3-/m0/s1</InChi>
+            # </chemComp>
+            old_records = ('<chemComp>', '<id>GLU</id>', '<name>GLUTAMIC ACID</name>', '<formula>C5 H9 N O4</formula>',
+                           '<systematicName>(2S)-2-azanylpentanedioic acid</systematicName>',
+                           '<stereoSmiles>N[C@@H](CCC(O)=O)C(O)=O</stereoSmiles>',
+                           '<nonStereoSmiles>N[CH](CCC(O)=O)C(O)=O</nonStereoSmiles>',
+                           '<InChi>InChI=1S/C5H9NO4/c6-3(5(9)10)1-2-4(7)8/h3H,1-2,6H2,(H,7,8)(H,9,10)/t3-/m0/s1'
+                           '</InChi>',
+                           '</chemComp>')
+        ccd = PdbChemicalComponentsRDKit(file_name=cif_filename(chem_comp_id))
+        chem_comp_xml = ccd.chem_comp_xml()
+        lines = chem_comp_xml.splitlines()
+        any_fail = False
+        for old_record in old_records:
+            match = False
+            for line in lines:
+                if old_record in line:
+                    match = True
+                    break
+            yield assert_true, match, '{} chem_comp_xml should contain "{}"'.format(chem_comp_id, old_record)
+            if not match:
+                any_fail = True
+        yield assert_false, any_fail, 'echo {} chem_comp_xml after any failure: "{}"'.\
+            format(chem_comp_id, chem_comp_xml)
