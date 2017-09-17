876da0703287671773ec96e32ce65792a41f4fee
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-09-17 21:27:05 +0100

    package setup: set version back to 0.0.1 and attempt to tag as a release.
    following:
    https://python-packaging.readthedocs.io/en/latest/dependencies.html
    for pip.
    
    For issue #18: Package code so it can be installed with pip
    
    Note yet tested!

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pdbeccdutils',
-      version='0.0.4',
+      version='0.0.1',
       description='Tools to deal with wwPDB chemical components definitions for small molecules',
       url='https://gitlab.com/pdbe/ccd_utils',
       author='Protein Data Bank in Europe',
