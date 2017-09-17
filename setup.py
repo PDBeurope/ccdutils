a6d8dcff2e578e9983f4629459ee9a8fb076213c
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-09-17 11:59:46 +0100

    package setup - include the fragment library
    following
    https://python-packaging.readthedocs.io/en/latest/non-code-files.html
    
    Does not have any dependencies yet.
    
    For issue #18: Package code so it can be installed with pip
    
    Tested by:
        (test-PDBeCIF) osmart@fuji01vm:~/ccd_utils$ pip install .
        Processing /home/osmart/ccd_utils
        Installing collected packages: pdbeccdutils
          Running setup.py install for pdbeccdutils ... done
        Successfully installed pdbeccdutils-0.0.2
        (test-PDBeCIF) osmart@fuji01vm:~/ccd_utils$ pip show -f pdbeccdutils
        Name: pdbeccdutils
        Version: 0.0.2
        ...
        Location: /mnt/extra/osmart/anaconda/envs/test-PDBeCIF/lib/python3.6/site-packages
        Requires:
        Files:
        ...
          pdbeccdutils/data/fragment_library.smi
    
    data/fragment_library.smi is picked up.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pdbeccdutils',
-      version='0.1',
+      version='0.0.2',
       description='Tools to deal with wwPDB chemical components definitions for small molecules',
       url='https://gitlab.com/pdbe/ccd_utils',
       author='Protein Data Bank in Europe',
       author_email='pdbehelp@ebi.ac.uk',
       license='Apache License 2.0.',
       packages=['pdbeccdutils'],
-      zip_safe=False)
+      zip_safe=False,
+      include_package_data=True)
