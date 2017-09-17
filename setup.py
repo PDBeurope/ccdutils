1f98e76477e8a4764841ac7909f0d3655afe33ec
Oliver Smart <oliver.smart@ebi.ac.uk>
2017-09-17 11:10:51 +0100

    package setup config file setup.py initial version following
    https://python-packaging.readthedocs.io/en/latest/minimal.html
    
    Does not have any dependencies yet.
    
    For issue #18: Package code so it can be installed with pip
    
    Tested by:
        (test-PDBeCIF) osmart@fuji01vm:~/ccd_utils$ pip install -e .
        Obtaining file:///home/osmart/ccd_utils
        Installing collected packages: pdbeccdutils
          Running setup.py develop for pdbeccdutils
        Successfully installed pdbeccdutils
    
    After this can use the module by:
    
        (test-PDBeCIF) osmart@fuji01vm:~$ python
        Python 3.6.2 |Continuum Analytics, Inc.| (default, Jul 20 2017, 13:51:32)
        [GCC 4.4.7 20120313 (Red Hat 4.4.7-1)] on linux
        Type "help", "copyright", "credits" or "license" for more information.
        >>> import pdbeccdutils
        >>> pdbeccdutils.pdb_chemical_components.__file__
        '/home/osmart/ccd_utils/pdbeccdutils/pdb_chemical_components.py'
        >>>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pdbeccdutils',
+      version='0.1',
+      description='Tools to deal with wwPDB chemical components definitions for small molecules',
+      url='https://gitlab.com/pdbe/ccd_utils',
+      author='Protein Data Bank in Europe',
+      author_email='pdbehelp@ebi.ac.uk',
+      license='Apache License 2.0.',
+      packages=['pdbeccdutils'],
+      zip_safe=False)
