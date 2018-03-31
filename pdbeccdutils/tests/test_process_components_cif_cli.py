948c1a1fb996795b6f4b0a1a8c9b3ae4594c1fdd
Oliver Smart <oliver.smart@ebi.ac.uk>
2018-03-31 14:39:11 +0100

    Add to refactoring branch files for unit test of process_components_cif_cli from master.
    Unit test fails because process_components_cif_cli code not yet added back to branch:
    
        $ pytest pdbeccdutils/tests/
        ==================================================================== test session starts ====================================================================
        platform darwin -- Python 3.6.1, pytest-3.5.0, py-1.5.3, pluggy-0.6.0
        rootdir: /Users/osmart/ccd_utils, inifile:
        collected 0 items / 1 errors
    
        ========================================================================== ERRORS ===========================================================================
        __________________________________________ ERROR collecting pdbeccdutils/tests/test_process_components_cif_cli.py ___________________________________________
        ImportError while importing test module '/Users/osmart/ccd_utils/pdbeccdutils/tests/test_process_components_cif_cli.py'.
        Hint: make sure your test modules/packages have valid Python names.
        Traceback:
        pdbeccdutils/tests/test_process_components_cif_cli.py:13: in <module>
            from pdbeccdutils.process_components_cif_cli import create_parser, process_components_cif
        E   ModuleNotFoundError: No module named 'pdbeccdutils.process_components_cif_cli'

---
+++ pdbeccdutils/tests/test_process_components_cif_cli.py
@@ -0,0 +1,170 @@
+"""
+Unittest of the process_components_cif_cli.py command line script
+
+method based on
+http://dustinrcollins.com/testing-python-command-line-apps
+adapted to use nose then converted to pytest
+"""
+import glob
+import os
+import pytest
+import shutil
+
+from pdbeccdutils.process_components_cif_cli import create_parser, process_components_cif
+from pdbeccdutils.tests.tst_utilities import test_cut_down_components_cif, \
+    file_name_in_tsts_out, cif_filename
+
+FILES_SUBDIRS = ('mmcif', 'sdf', 'sdf_nh', 'sdf_r', 'sdf_r_nh',
+                 'pdb', 'pdb_r', 'cml', 'xyz', 'xyz_r')
+IMAGES_SUBDIRS = 'svg_with_atom_labels', 'svg_without_atom_labels'
+
+
+class TestCommandLineArgs(object):
+    @staticmethod
+    def test_with_empty_args():
+        """
+        User passes no args, should produce a usage statement and then
+        raise SystemExit. Usage statement will appear
+        """
+        parser = create_parser()
+        with pytest.raises(SystemExit):
+            parser.parse_args([])
+
+    @staticmethod
+    def test_input_file_that_cannot_exist_raises_system_exit():
+        parser = create_parser()
+        args = parser.parse_args(['/////impossible_to_open_file', '--debug'])
+        with pytest.raises(SystemExit):
+            process_components_cif(args)
+
+
+class TestRegressionTest(object):
+    @staticmethod
+    def test_with_problematic_cif_7om():
+        """ 7OM caused problems because it lacks an inchikey
+        and this caused a crash """
+        cif_file = cif_filename('7OM')
+        test_output_dir = file_name_in_tsts_out('test_process_components_cif_70M')
+        if os.path.isdir(test_output_dir):
+            shutil.rmtree(test_output_dir)
+        parser = create_parser()
+        args = parser.parse_args([cif_file, '-o', test_output_dir])
+        process_components_cif(args)
+        assert os.path.isdir(test_output_dir), \
+            'output directory  {} must be created'.format(test_output_dir)
+
+
+class TestCutDownComponentsCif(object):
+    """
+    run process_components_cif_cli on test file:
+
+    cut_down_components.cif
+
+    that is a cutdown components cif with just the first 5 chemical
+    components definitions.
+
+    use pytest fixture to do the run once but then have separate tests
+    for the creation of each directory file that is required in the
+    ftp area.
+    """
+    CHEM_COMP_IDS = ('000', '001', '002', '003', '004')
+
+    @pytest.fixture(scope='class')
+    def output_dir(self):
+        parser = create_parser()
+        test_components_cif = test_cut_down_components_cif
+        output_dir = file_name_in_tsts_out('test_process_components_cif')
+        if os.path.isdir(output_dir):
+            shutil.rmtree(output_dir)
+        args = parser.parse_args([test_components_cif, '-o', output_dir])
+        process_components_cif(args)
+        return output_dir
+
+    @staticmethod
+    def test_output_dir_created(output_dir):
+        assert os.path.isdir(output_dir)
+
+    @staticmethod
+    def test_subdir_files_created(output_dir):
+        files_path = os.path.join(output_dir, 'files')
+        assert os.path.isdir(files_path)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', FILES_SUBDIRS)
+    def test_subdir_in_files_created(output_dir, subdir):
+        subdir_path = os.path.join(output_dir, 'files', subdir)
+        assert os.path.isdir(subdir_path)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', FILES_SUBDIRS)
+    @pytest.mark.parametrize('chem_comp_id', CHEM_COMP_IDS)
+    def test_there_is_a_file_for_chem_comp_id(output_dir, subdir, chem_comp_id):
+        subdir_path = os.path.join(output_dir, 'files', subdir)
+        assert os.path.isdir(subdir_path)
+        # simple check that there is a single file starting with the chem_comp_id
+        files_for_chem_comp_id = glob.glob1(subdir_path, chem_comp_id + '*')
+        assert len(files_for_chem_comp_id) == 1
+
+    @staticmethod
+    def test_subdir_images_created(output_dir):
+        files_path = os.path.join(output_dir, 'images')
+        assert os.path.isdir(files_path)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', IMAGES_SUBDIRS)
+    def test_subdir_in_files_created(output_dir, subdir):
+        subdir_path = os.path.join(output_dir, 'images', subdir)
+        assert os.path.isdir(subdir_path)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', IMAGES_SUBDIRS)
+    @pytest.mark.parametrize('chem_comp_id', CHEM_COMP_IDS)
+    def test_there_is_a_svg_for_chem_comp_id(output_dir, subdir, chem_comp_id):
+        subdir_path = os.path.join(output_dir, 'images', subdir, chem_comp_id[:1])
+        assert os.path.isdir(subdir_path)
+        this_svg = os.path.join(subdir_path, chem_comp_id + '.svg')
+        assert os.path.isfile(this_svg) and os.path.getsize(this_svg) > 0
+
+    def test_file_chem_comp_dot_list(self, output_dir):
+        chem_comp_dot_list_file = os.path.join(output_dir, 'chem_comp.list')
+        try:
+            with open(chem_comp_dot_list_file, 'r') as chem_comp_file:
+                lines = chem_comp_file.read().splitlines()
+                assert lines == list(self.CHEM_COMP_IDS), \
+                    'chem_comp.list file should contain list of ccd''s one per line'
+        except IOError as message:
+            assert False, 'problem opening chem_comp.list file "{}"'.format(message)
+
+    @staticmethod
+    def test_file_chem_dot_xml(output_dir):
+        chem_dot_xml_file_name = os.path.join(output_dir, 'chem.xml')
+        try:
+            with open(chem_dot_xml_file_name, 'r') as chem_dot_xml_file:
+                lines = chem_dot_xml_file.read().splitlines()
+                strip_lines = [item.strip() for item in lines]
+                for test_str in ('</chemCompList>', '<id>000</id>', '<id>004</id>',
+                                 '<name>(2S)-amino(phenyl)ethanoic acid</name>',
+                                 '<fragment id="2" name="phenyl">'):
+                    assert test_str in strip_lines, 'chem.xml should contain {}'.format(test_str)
+        except IOError as message:
+            assert False, 'problem opening chem.xml file "{}"'.format(message)
+
+    @staticmethod
+    def test_file_readme_dot_html_file(output_dir):
+        this_file = os.path.join(output_dir, 'readme.htm')
+        assert os.path.isfile(this_file) and os.path.getsize(this_file) > 0, \
+            'readme_dot_html_file {} must be a non-empty file.'.format(this_file)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', FILES_SUBDIRS)
+    def test_tarball_in_files(output_dir, subdir):
+        this_file = os.path.join(output_dir, 'files', subdir + '.tar.gz')
+        assert os.path.isfile(this_file) and os.path.getsize(this_file) > 0, \
+            'tarball {} must be a non-empty file.'.format(this_file)
+
+    @staticmethod
+    @pytest.mark.parametrize('subdir', IMAGES_SUBDIRS)
+    def test_tarball_in_images(output_dir, subdir):
+        this_file = os.path.join(output_dir, 'images', subdir + '.tar.gz')
+        assert os.path.isfile(this_file) and os.path.getsize(this_file) > 0, \
+            'tarball {} must be a non-empty file.'.format(this_file)
---
+++ pdbeccdutils/tests/tst_utilities.py
@@ -0,0 +1,72 @@
+# software from PDBe: Protein Data Bank in Europe; http://pdbe.org
+#
+# Copyright 2017 EMBL - European Bioinformatics Institute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+import glob
+import os
+
+
+def tests_dir():
+    """
+   directory of the tests directory where this python file is.
+
+    Returns:
+        str the name of the directory is
+
+    """
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+test_cif_path_name = os.path.join(tests_dir(), 'ccd_mmcif_test_files')
+test_comparison_files_path = os.path.join(tests_dir(), 'comparison_files')
+test_cut_down_components_cif = os.path.join(tests_dir(), 'components_cif',
+                                                    'cut_down_components.cif')
+
+
+def cif_filename(code):
+    return os.path.join(test_cif_path_name, code + '.cif')
+
+
+def supply_list_of_sample_cifs():
+    """
+    returns the list of sample pdb ccd cifs for test.
+
+    Args:
+
+    Returns:
+        list of filenames
+    """
+    return sorted(glob.glob(os.path.join(test_cif_path_name, '*.cif')))
+
+
+def file_name_in_tsts_out(file_name, remove_existing=True):
+    """
+    creates the subdirectory "out" in the "tests" subdirectory (if necessary)
+    and returns the file_name in this directory, If the file already exists it will remove it.
+    (Cannot call method file_name_in_tests_out otherwise nose thinks it is a test)
+
+    Args:
+        file_name (str):  the name for the file
+        remove_existing (bool): remove existing file?
+
+    Returns:
+        str: the filename in the subdirectory tests/out
+    """
+    subdir = os.path.join(tests_dir(), 'out')
+    if not os.path.isdir(subdir):
+        os.mkdir(subdir)
+    out_file_name = os.path.join(subdir, file_name)
+    if remove_existing and os.path.isfile(out_file_name):
+        os.remove(out_file_name)
+    return out_file_name
