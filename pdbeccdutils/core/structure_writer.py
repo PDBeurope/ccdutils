7d91a1c7499aaa869d2dffd2cbbe9ca40a286eb6
Lukas Pravda <lpravda@ebi.ac.uk>
2018-04-22 10:56:46 +0200

    Basic support of XML and XYZ export
    not extensivelly tested.

--- pdbeccdutils/core/structure_writer.py
+++ pdbeccdutils/core/structure_writer.py
@@ -1,19 +1,37 @@
-from pdbeccdutils.core import Component
-from pdbeccdutils.core import ConformerType
-from rdkit import Chem
-from rdkit import rdBase
+#!/usr/bin/env python
+# software from PDBe: Protein Data Bank in Europe; https://pdbe.org
+#
+# Copyright 2018 EMBL - European Bioinformatics Institute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
+from rdkit import Chem, rdBase
+
+from pdbeccdutils.core import Component, ConformerType
+
 
 def write_molecule(path, component, remove_hs=True, conf_type=ConformerType.Ideal):
-    """[summary]
+    """Export molecule in a specified format.
 
     Args:
-        path ([type]): [description]
-        component ([type]): [description]
-        remove_hs (bool, optional): Defaults to True. [description]
-        conf_type ([type], optional): Defaults to ConformerType.Ideal. [description]
+        path (str): Path to the file. Extension determines format to be
+            used.
+        component (pdbeccdutils.core.Component): Component to be exported
+        remove_hs (bool, optional): Defaults to True.
+        conf_type ([type], optional): Defaults to ConformerType.Ideal.
 
     Raises:
         NotImplementedError: For format exports under development
@@ -27,11 +45,13 @@ def write_molecule(path, component, remove_hs=True, conf_type=ConformerType.Idea
     elif extension == 'pdb':
         str_representation = to_pdb_str(component, remove_hs, conf_type)
     elif extension in ('mmcif', 'cif'):
-        raise str_representation = to_mmcif_str(component, remove_hs, conf_type)
+        str_representation = to_mmcif_str(component, remove_hs, conf_type)
     elif extension == 'cml':
         str_representation = to_cml_str(component, remove_hs, conf_type)
     elif extension == 'xml':
         str_representation = to_xml_str(component, remove_hs, conf_type)
+    elif extension == 'xyz':
+        str_representation = to_xyz_str(component, remove_hs, conf_type)
     else:
         raise ValueError('Unsupported file format: {}'.format(extension))
 
@@ -95,8 +115,92 @@ def to_sdf_str(component, remove_hs=True, conf_type=ConformerType.Ideal):
     return Chem.MolToMolBlock(mol_to_save, confId=conf_id)
 
 
+def to_xyz_str(component, remove_hs=True, conf_type=ConformerType.Ideal):
+    """Converts structure to the XYZ format.
+
+    Args:
+        component (pdbeccdutils.core.Component): Component to be
+            exported.
+        remove_hs (bool, optional): Defaults to True.
+        conf_type (pdbeccdutils.core.ConformerType, optional):
+            Defaults to ConformerType.Ideal.
+
+    Returns:
+        str: String representation of the component in the XYZ format
+    """
+    (mol_to_save, conf_id, conf_type) = _prepate_structure(component, remove_hs, conf_type)
+    conformer = mol_to_save.GetConformer(id=conf_id)
+
+    result = list()
+    result.append(str(mol_to_save.GetNumAtoms()))
+    result.append(component.id)
+
+    for atom in mol_to_save.GetAtoms():
+        coords = conformer.GetAtomPosition(atom.GetIdx())
+        result.append('{0:<4}{1: f}  {2: f}  {3: f}'.
+                      format(atom.GetSymbol(), coords.x, coords.y, coords.z))
+
+    return '\n'.join(result)
+
+
+def to_xml_xml(component, remove_hs=True, conf_type=ConformerType.Ideal):
+    """Converts structure to the XML format and returns its XML repr.
+
+    Args:
+        component (pdbeccdutils.core.Component): Component to be
+            exported.
+        remove_hs (bool, optional): Defaults to True.
+        conf_type (pdbeccdutils.core.ConformerType, optional):
+            Defaults to ConformerType.Ideal.
+
+    Returns:
+        xml.etree.ElementTree.Element: XML object
+    """
+    root = ET.Element('chemComp')
+
+    id_e = ET.SubElement(root, 'id')
+    name_e = ET.SubElement(root, 'name')
+    formula_e = ET.SubElement(root, 'formula')
+    sys_name_e = ET.SubElement(root, 'systematicName')
+    s_smiles_e = ET.SubElement(root, 'stereoSmiles')
+    n_smiles_e = ET.SubElement(root, 'nonStereoSmiles')
+    inchi_e = ET.SubElement(root, 'inchi')
+
+    name_e.text = component.name
+    id_e.text = component.id
+    formula_e.text = component.formula
+    sys_name_e.text = next((x.value for x in component.descriptors
+                            if x.type == 'SYSTEMATIC NAME' and x.program == 'ACDLabs'), '')
+    s_smiles_e.text = next((x.value for x in component.descriptors
+                            if x.type == 'SMILES_CANONICAL' and x.program == 'CACTVS'), '')
+    n_smiles_e.text = next((x.value for x in component.descriptors
+                            if x.type == 'SMILES' and x.program == 'CACTVS'), '')
+    inchi_e.text = component.inchi
+
+    return root
+
+
 def to_xml_str(component, remove_hs=True, conf_type=ConformerType.Ideal):
-    raise NotImplementedError()
+    """Converts structure to the XML format. Presently just molecule
+    metadata are serialized without any coordinates, which is in
+    accordance with the content of the PDBeChem area.
+
+    Args:
+        component (pdbeccdutils.core.Component): Component to be
+            exported.
+        remove_hs (bool, optional): Defaults to True.
+        conf_type (pdbeccdutils.core.ConformerType, optional):
+            Defaults to ConformerType.Ideal.
+
+    Returns:
+        str: String representation of the component in CML format.
+    """
+    root = to_xml_xml(component, remove_hs, conf_type)
+
+    xml = ET.tostring(root, encoding='utf-8', method='xml')
+    pretty = minidom.parseString(xml)
+
+    return pretty.toprettyxml(indent="  ")
 
 
 def to_mmcif_str(component, remove_hs=True, conf_type=ConformerType.Ideal):
