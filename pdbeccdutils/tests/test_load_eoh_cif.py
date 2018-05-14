1bf548523373c075a21454e34f81fc16b754053a
Oliver Smart <oliver.smart@ebi.ac.uk>
2018-05-14 16:21:03 +0100

    start to parametrize test_load_eoh_cif.
    for issue #37
    Test on fails "released" because component.released and has altered in
    commit 3c45ddac and is no longer True:
    
        $ pytest tests/test_load_eoh_cif.py  -v
        =========================================================== test session starts ============================================================
        platform darwin -- Python 3.6.1, pytest-3.5.0, py-1.5.3, pluggy-0.6.0 -- /Users/osmart/anaconda3/envs/test-rdkit/bin/python
        cachedir: ../.pytest_cache
        rootdir: /Users/osmart/ccd_utils, inifile:
        collected 4 items
    
        tests/test_load_eoh_cif.py::TestLoadEOH::test_chem_comp_items[id-EOH] PASSED                                                         [ 25%]
        tests/test_load_eoh_cif.py::TestLoadEOH::test_chem_comp_items[name-ETHANOL] PASSED                                                   [ 50%]
        tests/test_load_eoh_cif.py::TestLoadEOH::test_chem_comp_items[formula-C2 H6 O] PASSED                                                [ 75%]
        tests/test_load_eoh_cif.py::TestLoadEOH::test_chem_comp_items[released-True] FAILED                                                  [100%]
    
        ================================================================= FAILURES =================================================================
        _____________________________________________ TestLoadEOH.test_chem_comp_items[released-True] ______________________________________________
    
        attribute = 'released', expected = True
    
            @staticmethod
            @pytest.mark.parametrize('attribute, expected', chem_comp_items)
            def test_chem_comp_items(attribute, expected):
                """ for EOH.cif tests items from _chem_comp:
                        _chem_comp.id                                    EOH
                        _chem_comp.name                                  ETHANOL
                        _chem_comp.formula                               "C2 H6 O"
                        _chem_comp.pdbx_release_status                   REL
                        and that released is True
                    """
                cif_file = cif_filename('EOH')
                reader = ccd_reader.read_pdb_cif_file(cif_file)
                assert reader.warnings == []
                assert reader.errors == []
                component = reader.component
                assert hasattr(component, attribute)
                if type(expected) is type(True):
        >           assert getattr(component, attribute) is expected
        E           AssertionError: assert <ReleaseStatus.REL: 1> is True
        E            +  where <ReleaseStatus.REL: 1> = getattr(<pdbeccdutils.core.component.Component object at 0x106f15eb8>, 'released')
    
        tests/test_load_eoh_cif.py:32: AssertionError
        ==================================================== 1 failed, 3 passed in 0.83 seconds ====================================================

--- pdbeccdutils/tests/test_load_eoh_cif.py
+++ pdbeccdutils/tests/test_load_eoh_cif.py
@@ -1,20 +1,34 @@
 """
 load EOH.cif from file and test important cif item.
 """
+import pytest
 from pdbeccdutils.tests.tst_utilities import cif_filename
 from pdbeccdutils.core import ccd_reader
 
 class TestLoadEOH:
+    chem_comp_items = [('id', 'EOH'),
+                       ('name', 'ETHANOL' ),
+                       ('formula', 'C2 H6 O'),
+                       # ('pdbx_release_status', 'REL'), TODO
+                       ('released', True)
+                       ]
     @staticmethod
-    def test_load_eoh():
-        """ test eoh?? """
+    @pytest.mark.parametrize('attribute, expected', chem_comp_items)
+    def test_chem_comp_items(attribute, expected):
+        """ for EOH.cif tests items from _chem_comp:
+            _chem_comp.id                                    EOH
+            _chem_comp.name                                  ETHANOL
+            _chem_comp.formula                               "C2 H6 O"
+            _chem_comp.pdbx_release_status                   REL
+            and that released is True
+        """
         cif_file = cif_filename('EOH')
         reader = ccd_reader.read_pdb_cif_file(cif_file)
         assert reader.warnings == []
         assert reader.errors == []
         component = reader.component
-        assert component.id == 'EOH'
-        assert component.name == 'ETHANOL'
-        assert component.formula == 'C2 H6 O'
-        # really want to test _chem_comp.pdbx_release_status
-        assert component.released == True
+        assert hasattr(component, attribute)
+        if type(expected) is type(True):
+            assert getattr(component, attribute) is expected
+        else:
+            assert getattr(component, attribute) == expected
