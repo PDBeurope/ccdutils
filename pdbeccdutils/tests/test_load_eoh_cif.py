c449515304f59e3871556144fe43a027cda18a5b
Oliver Smart <oliver.smart@ebi.ac.uk>
2018-05-16 11:47:17 +0100

    Refactor tests to use fixture for loading eoh for issue #37

--- pdbeccdutils/tests/test_load_eoh_cif.py
+++ pdbeccdutils/tests/test_load_eoh_cif.py
@@ -8,31 +8,60 @@ from pdbeccdutils.core import ReleaseStatus
 
 
 class TestLoadEOH:
-    chem_comp_items = [('id', 'EOH'),
-                       ('name', 'ETHANOL' ),
-                       ('formula', 'C2 H6 O'),
-                       ('pdbx_release_status', ReleaseStatus.REL),
-                       ('released', True),
-                       ('inchikey', 'LFQSCWFLJHTTHZ-UHFFFAOYSA-N'),
 
-                       ]
     @staticmethod
-    @pytest.mark.parametrize('attribute, expected', chem_comp_items)
-    def test_chem_comp_items(attribute, expected):
-        """ for EOH.cif tests items from _chem_comp:
-            _chem_comp.id                                    EOH
-            _chem_comp.name                                  ETHANOL
-            _chem_comp.formula                               "C2 H6 O"
-            _chem_comp.pdbx_release_status                   REL
-            and that released is True
+    @pytest.fixture(scope='class')
+    def component_eoh():
+        """
+        loads PDB-CCD for ethanol eoh.cif and returns the
+        component object
+
+        Returns:
+            pdbeccdutils.core.component.Component: component object for
+            eoh.cif
         """
         cif_file = cif_filename('EOH')
         reader = ccd_reader.read_pdb_cif_file(cif_file)
         assert reader.warnings == []
         assert reader.errors == []
         component = reader.component
-        assert hasattr(component, attribute)
-        if type(expected) is type(True):
-            assert getattr(component, attribute) is expected
-        else:
-            assert getattr(component, attribute) == expected
+        return component
+
+    @staticmethod
+    def test_chem_comp_dot_id(component_eoh):
+        """test retrieval of _chem_comp.id EOH"""
+        assert component_eoh.id == 'EOH'
+
+    chem_comp_items = [('id', 'EOH'),
+                       ('name', 'ETHANOL'),
+                       ('formula', 'C2 H6 O'),
+                       ('pdbx_release_status', ReleaseStatus.REL),
+                       ]
+
+    @staticmethod
+    @pytest.mark.parametrize('attribute, expected', chem_comp_items)
+    def test_chem_comp_items(attribute, expected, component_eoh):
+        """ for EOH.cif tests items from _chem_comp:
+            _chem_comp.id                                    EOH
+            _chem_comp.name                                  ETHANOL
+            _chem_comp.formula                               "C2 H6 O"
+            _chem_comp.pdbx_release_status                   REL
+        """
+        assert hasattr(component_eoh, attribute)
+        assert getattr(component_eoh, attribute) == expected
+
+    @staticmethod
+    def test_released_is_true(component_eoh):
+        assert component_eoh.released is True
+
+    @staticmethod
+    def test_inchikey(component_eoh):
+        """
+        test InChIKey retrieval from _pdbx_chem_comp_descriptor line:
+        EOH InChIKey InChI 1.03  LFQSCWFLJHTTHZ-UHFFFAOYSA-N
+        """
+        assert component_eoh.inchikey == 'LFQSCWFLJHTTHZ-UHFFFAOYSA-N'
+
+    @staticmethod
+    def test_eoh_has_nine_atoms(component_eoh):
+        assert component_eoh.mol.GetNumAtoms() == 9
