5ce43b86370b779d874d465d9d358a83717f4bcd
Oliver Smart <oliver.smart@ebi.ac.uk>
2018-05-22 11:37:41 +0100

    Check sdf writing for each of the sample cifs add model and without/with hydrogen atoms. Tests pass.
    for issue #37

--- pdbeccdutils/tests/test_coordinate_file_writing.py
+++ pdbeccdutils/tests/test_coordinate_file_writing.py
@@ -3,6 +3,7 @@ import pytest
 from pdbeccdutils.tests.tst_utilities import supply_list_of_sample_cifs, file_name_in_tsts_out
 from pdbeccdutils.core import ccd_reader
 from pdbeccdutils.core import structure_writer
+from pdbeccdutils.core import ConformerType
 
 
 sample_ccd_cifs = supply_list_of_sample_cifs()
@@ -15,16 +16,31 @@ class TestSDF:
         reader = ccd_reader.read_pdb_cif_file(test_ccd_cif)
         assert reader.errors == []
         component = reader.component
-        sdf_file = file_name_in_tsts_out(component.id + '.ideal_withH.sdf')
-        if os.path.isfile(sdf_file):
-            os.remove(sdf_file)
-        if component.id in ('10R', 'UNL'): # known problem codes.
-            with pytest.raises(ValueError):
-                structure_writer.write_molecule(path=sdf_file, component=component,
+        for ideal in True, False:
+            for remove_hs in True, False:
+                sdf_file = file_name_in_tsts_out(component.id)
+                if ideal:
+                    sdf_file += '_ideal'
+                    conf_type = ConformerType.Ideal
+                else:
+                    sdf_file += '_model'
+                    conf_type = ConformerType.Model
+                if remove_hs:
+                    sdf_file += '_no_h'
+                else:
+                    sdf_file += '_with_h'
+                sdf_file += '.sdf'
+                if os.path.isfile(sdf_file):
+                    os.remove(sdf_file)
+                if component.id in ('10R', 'UNL'): # known problem codes.
+                    with pytest.raises(ValueError):
+                        structure_writer.write_molecule(path=sdf_file,
+                                                        component=component,
+                                                        conf_type=conf_type,
+                                                        remove_hs=remove_hs)
+                else:
+                    structure_writer.write_molecule(path=sdf_file, component=component,
                                                 remove_hs=False)
-        else:
-            structure_writer.write_molecule(path=sdf_file, component=component,
-                                        remove_hs=False)
-            assert os.path.isfile(sdf_file)
-            assert os.path.getsize(sdf_file) > 0
+                    assert os.path.isfile(sdf_file)
+                    assert os.path.getsize(sdf_file) > 0
 
