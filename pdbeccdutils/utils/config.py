4d9e8b34b99e0b640153e58e7caafc1afee15474
Lukas Pravda <lpravda@ebi.ac.uk>
2018-05-23 11:55:31 +0100

    Add package configuration
    This way pdbeccdutils data can be directly accessed. Presently there are two properties:
    
    pdbeccdutils.utils.config.general_templates
    pdbeccdutils.utils.config.fragment_library

---
+++ pdbeccdutils/utils/config.py
@@ -0,0 +1,5 @@
+import os
+import pdbeccdutils
+
+general_templates = os.path.join(os.path.dirname(pdbeccdutils.__file__), 'data', 'general_templates')
+fragment_library = os.path.join(os.path.dirname(pdbeccdutils.__file__), 'fragment_library.tsv')
