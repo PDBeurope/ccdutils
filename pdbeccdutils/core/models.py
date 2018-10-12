97ada621b4e381a83467cc3b312b91cb38d7755d
abhik <abhik@ebi.ac.uk>
2018-10-12 15:46:44 +0100

    add scaffolding support

--- pdbeccdutils/core/models.py
+++ pdbeccdutils/core/models.py
@@ -69,6 +69,16 @@ class ReleaseStatus(IntEnum):
     REF_ONLY = 5
     REL = 6
 
+class ScaffoldingMethod(IntEnum):
+    """
+    Rdkit scaffold methods
+    """
+
+    Murcko_scaffold = 1
+    Murcko_generic = 2
+    Brics = 3
+
+
 
 DepictionResult = NamedTuple('DepictionResult',
                              [('source', DepictionSource),
