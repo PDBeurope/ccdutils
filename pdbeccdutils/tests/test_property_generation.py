42ed80ef586e9adaae33cddfdbf480aee45cdf9f
Lukas Pravda <lpravda@ebi.ac.uk>
2018-11-19 13:20:47 +0000

    Change properties testing scenario

--- pdbeccdutils/tests/test_property_generation.py
+++ pdbeccdutils/tests/test_property_generation.py
@@ -1,32 +1,29 @@
 import pytest
 
-from rdkit import Chem
-
-from pdbeccdutils.tests.tst_utilities import cif_filename
 from pdbeccdutils.core import ccd_reader
+from pdbeccdutils.tests.tst_utilities import cif_filename
+
+test_inputs = {
+    'NAG': {
+        'logp': -3.078,
+        'heavy_atom_count': 26
+    }
+}
+
+# ('numH_acceptors', '4'),
+# ('numH_donors', '2'),
+# ('num_rotable_bonds', '7'),
+# ('rings_count', '3'),
+# ('TPSA', '66.75999999999999'),
+# ('molwt', '352.4299999999996')]
 
 
-"""
-test of the property generation of hetcode NAG
-"""
+class TestPropertyCalculation:
 
-test_inputs = [('NAG', 'logp_v', -3.078)]
-               # ('heavy_atom_count', '26'),
-               # ('numH_acceptors', '4'),
-               # ('numH_donors', '2'),
-               # ('num_rotable_bonds', '7'),
-               # ('rings_count', '3'),
-               # ('TPSA', '66.75999999999999'),
-               # ('molwt', '352.4299999999996')]
-
-class TestIn:
     @staticmethod
-    @pytest.mark.parametrize('id, logp_v, value', test_inputs)
-    def test_property_calculation(id, logp_v, value):
-
+    @pytest.mark.parametrize('id', test_inputs)
+    def test_properties(id):
         component = ccd_reader.read_pdb_cif_file(cif_filename(id)).component
 
-        result = component.properties.logP
-        assert result == value
-
-
+        assert test_inputs[id]['logp'] == round(component.properties.logP, 3)
+        # TODO other tests in a similar manner as before
