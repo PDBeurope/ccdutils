5909bb610a4fa9e4bb9952dc3bfc8e7da75dee06
Lukas Pravda <lpravda@ebi.ac.uk>
2019-12-05 10:46:50 +0000

    correctly add CSS styling to documentation

--- doc/conf.py
+++ doc/conf.py
@@ -108,6 +108,7 @@ html_logo = "logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['css/styles.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
