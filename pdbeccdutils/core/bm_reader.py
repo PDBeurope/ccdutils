aa877b62722c928cc8f1731ab35c1bb34c4faf40
roshkjr <rosh.k.j.r@gmail.com>
2023-05-03 15:04:38 +0100

    added copyright info

--- pdbeccdutils/core/bm_reader.py
+++ pdbeccdutils/core/bm_reader.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# software from PDBe: Protein Data Bank in Europe; https://pdbe.org
+#
+# Copyright 2018 EMBL - European Bioinformatics Institute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+A set of methods for identifying bound-molecules (covalently bonded CCDs ) from mmCIF files
+of proteins and creating Component representation of molecules.
+"""
+
 import os
 import rdkit
 from collections import namedtuple
