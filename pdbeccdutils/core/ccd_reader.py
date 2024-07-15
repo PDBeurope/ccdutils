76a427c7d8f0167d2e978bc090757f3864516dda
roshan <roshan@ebi.ac.uk>
2024-07-15 13:13:27 +0100

    style: formating

--- pdbeccdutils/core/ccd_reader.py
+++ pdbeccdutils/core/ccd_reader.py
@@ -101,7 +101,7 @@ def read_pdb_cif_file(path_to_cif: str, sanitize: bool = True) -> CCDReaderResul
 
 
 def read_pdb_components_file(
-    path_to_cif: str, sanitize: bool = True, include: list[str]=[]
+    path_to_cif: str, sanitize: bool = True, include: list[str] = []
 ) -> Dict[str, CCDReaderResult]:
     """
     Process multiple compounds stored in the wwPDB CCD
@@ -112,7 +112,7 @@ def read_pdb_components_file(
             multiple ligands in it.
         sanitize (bool): Whether or not the components should be sanitized
             Defaults to True.
-        include (list[str]): List of CCDs to be parsed. By deafult it is empy and parse 
+        include (list[str]): List of CCDs to be parsed. By deafult it is empy and parse
         all the CCDs. If a list of CCDs provided, will only parse them
 
     Raises:
@@ -127,15 +127,14 @@ def read_pdb_components_file(
 
     result_bag = {}
     for block in cif.read(path_to_cif):
-        if (block.name in include) or (include.len()==0): 
+        if (block.name in include) or (include.len() == 0):
             try:
                 result_bag[block.name] = _parse_pdb_mmcif(block, sanitize)
             except CCDUtilsError as e:
                 logging.error(
                     f"ERROR: Data block {block.name} not processed. Reason: ({str(e)})."
-                )              
+                )
     return result_bag
-
 
 
 # region parse mmcif
